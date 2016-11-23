c4de09519a60dd151abee27a0c027918ac2c5f4b
Luis José Sánchez <luisjoseprofe@gmail.com>
2016-11-23 17:43:49 +0100

    Car class

---
+++ Car.ts
@@ -0,0 +1,16 @@
+class Car {
+  engine: string;
+
+  constructor(engine: string) {
+    this.engine = engine;
+  }
+
+  start() {
+    alert('Engine started: ' + this.engine);
+  }
+
+
+  stop() {
+    alert('Engine stoped: ' + this.engine);
+  }
+}
